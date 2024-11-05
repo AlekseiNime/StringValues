8c8ce791adb731248c948793da62ce7b1d680add
alekseinime <mamagold1213@gmail.com>
2024-11-05 09:38:59 +0200

    Практикум №7

---
+++ ConsoleApp5/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Task1
+            //Console.Write("Напишите своё имя:");
+            //string a = Console.ReadLine();
+            //Console.WriteLine(a);
+            //Console.Write("Напишите своё имя:");
+            //string b = Console.ReadLine();
+            //Console.WriteLine($"Привет {b}");
+            //Console.ReadKey();
+
+            //Task2
+            //Console.Write("Напишите название футбольной команды: ");
+            //string a = Console.ReadLine();
+            //Console.WriteLine($"{a} - Это чемпион!");
+            //Console.ReadKey();
+
+            //Task3
+            //Console.Write("Введите Имя: ");
+            //string name = Console.ReadLine();
+            //Console.Write("Введите Фамилию: ");
+            //string surname = Console.ReadLine();
+            //Console.WriteLine($"{name}\t{surname}");
+            //Console.ReadKey();
+
+            //Task4
+
+            //Task5
+
+            //Task6
+            //Console.Write("Введите название футбольного клуба: ");
+            //string a = Console.ReadLine();
+            //Console.WriteLine($"{a.Length}");
+            //Console.ReadKey();
+
+            //Task7
+
+
+
+
+            //Task8
+            //Console.Write("Здравствуй, я компьютер, а тебя как зовут?: ");
+            //string a = Console.ReadLine();
+            //Console.WriteLine($"Очень приятно, {a}.");
+            //Console.Write("Сколько тебе лет?: ");
+            //int age = int.Parse(Console.ReadLine());
+            //Console.WriteLine($"Ого! Целых {age} лет! Ты уже совсем взрослый!");
+            //Console.ReadKey();
+
+            //Task9
+
+            //Task10
+            //Console.Write("Введите ваше Имя: ");
+            //string name = Console.ReadLine();
+            //Console.Write("Введите ваш возраст: ");
+            //int b = int.Parse(Console.ReadLine());
+            //Console.WriteLine($"{name}, ты родился в {2024 - b} году.");
+            //Console.ReadKey();
+
+        }
+    }
+}
