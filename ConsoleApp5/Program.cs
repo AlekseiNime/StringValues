2cef8231cf4e5ccce57fc6efa6130f97e4963cf7
AlekseiNime <mamagold1213@gmail.com>
2024-11-10 20:11:52 +0200

    Практикум №7

--- ConsoleApp5/Program.cs
+++ ConsoleApp5/Program.cs
@@ -34,8 +34,18 @@ namespace ConsoleApp5
             //Console.ReadKey();
 
             //Task4
+            //Console.Write("Введите название государства: ");
+            //string country = Console.ReadLine();
+            //Console.Write("Введите название столицы: ");
+            //string capital = Console.ReadLine();
+            //Console.WriteLine($"Столица государства {country} — город {capital}");
 
             //Task5
+            //Console.Write("Введите название романа: ");
+            //string novel = Console.ReadLine();
+            //Console.Write("Введите фамилию автора: ");
+            //string author = Console.ReadLine();
+            //Console.WriteLine($"Писатель {author} — автор романа {novel}");
 
             //Task6
             //Console.Write("Введите название футбольного клуба: ");
@@ -44,9 +54,21 @@ namespace ConsoleApp5
             //Console.ReadKey();
 
             //Task7
+            //Console.WriteLine("Ты вчера был болен.");
+            //Console.WriteLine("Измерь-ка температуру!");
+            //Console.Write("Сообщи, какая у тебя температура: ");
+            //double temp = Convert.ToDouble(Console.ReadLine());
 
-
-
+            //if (temp >= 36.5 && temp <= 37.5)
+            //{
+            //    Console.WriteLine("Ты здоров, дружок!");
+            //    Console.WriteLine("Можешь идти в школу.");
+            //    Console.WriteLine("Желаю успехов.");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Ты болен, отдыхай.");
+            //}
 
             //Task8
             //Console.Write("Здравствуй, я компьютер, а тебя как зовут?: ");
@@ -58,6 +80,19 @@ namespace ConsoleApp5
             //Console.ReadKey();
 
             //Task9
+            //Console.Write("Как тебя зовут? ");
+            //string name = Console.ReadLine();
+            //Console.WriteLine($"Привет, {name}!");
+            //Console.Write("Сколько тебе лет? ");
+            //int age = Convert.ToInt32(Console.ReadLine());
+            //Console.Write("Какой у тебя любимый предмет в техникуме? ");
+            //string Subject = Console.ReadLine();
+            //Console.Write("Какая у тебя любимая книга? ");
+            //string Book = Console.ReadLine();
+            //Console.WriteLine($"Значит тебя зовут, {name}.");
+            //Console.WriteLine($"Тебе {age} лет.");
+            //Console.WriteLine($"Твой любимый предмет - {Subject}.");
+            //Console.WriteLine($"И твоя любимая книга – {Book}.");
 
             //Task10
             //Console.Write("Введите ваше Имя: ");
